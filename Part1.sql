9fbb21737ef6f990f51bd2d5d6ba8d0d90a3059e
Esraa Elgebaly <105568236+esraalegebaly@users.noreply.github.com>
2024-05-20 03:07:56 +0300

    Add files via upload

---
+++ Part1.sql
@@ -0,0 +1,19 @@
+USE db_course_conversions;
+
+SELECT 
+    e.student_id,
+    i.date_registered,
+    MIN(e.date_watched) AS first_date_watched,
+    MIN(p.date_purchased) AS first_date_purchased,
+    DATEDIFF(MIN(e.date_watched), i.date_registered) AS days_diff_reg_watch,
+    DATEDIFF(MIN(p.date_purchased),
+            MIN(e.date_watched)) AS days_diff_watch_purch
+FROM
+    student_engagement e
+        JOIN
+    student_info i ON e.student_id = i.student_id
+        LEFT JOIN
+    student_purchases p ON e.student_id = p.student_id
+GROUP BY e.student_id
+HAVING first_date_purchased IS NULL
+    OR first_date_watched <= first_date_purchased;
